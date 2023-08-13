8d3c81d2dd3ab1d5d129ceb73af910ec5f6962ee
Tim Rogers <me@timrogers.co.uk>
2023-08-13 19:24:58 +0100

    chore: autocorrect linting failures

--- src/airports.tsx
+++ src/airports.tsx
@@ -136,7 +136,7 @@ function useSearch(): { state: AirportSearchState; search: (text: string) => voi
         showToast({ style: Toast.Style.Failure, title: "Oops! Something went wrong.", message: String(error) });
       }
     },
-    [cancelRef, setState]
+    [cancelRef, setState],
   );
 
   useEffect(() => {
